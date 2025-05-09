ea0c540886960508791e8b7039b004bb2ecd8b15
Faneva <Andrianirinafaneva94@gmail.com>
2025-05-09 07:24:11 +0300

    Configuration mode trunk interface S2 et S3

--- script.py
+++ script.py
@@ -1,9 +1,7 @@
 #!/home/faneva/Bureau/Networking/nenv/bin/python
 
-
-
-from dotenv import load_dotenv
 from netmiko import ConnectHandler, NetMikoAuthenticationException, NetMikoTimeoutException
+from dotenv import load_dotenv
 import time
 import os
 
@@ -13,49 +11,45 @@ USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 SECRET = os.getenv("SECRET")
 
-
 with open("ips.txt", "r") as file:
     ips = [ip.strip() for ip in file.readlines()]
 
 for ip in ips:
     try:
-        device = {
-            "device_type":"cisco_ios",
-            "host":ip,
-            "username":USERNAME,
-            "password":PASSWORD,
-            "secret":SECRET
-        }
         if ip in ["192.168.137.6", "192.168.137.7"]:
+            device = {
+                "device_type":"cisco_ios",
+                "host":ip,
+                "username":USERNAME,
+                "password":PASSWORD,
+                "secret":SECRET
+            }
+            
             print(f"Connecting to {ip}...")
-            net_con = ConnectHandler(**device)
 
-            print("Connected successfully")
-            net_con.enable()
+            net_conn = ConnectHandler(**device)
+            net_conn.enable()
+
+            print("Connected successfullly")
 
             cmd = [
-                "int range g1/2-3",
-                "channel-proto lacp",
-                "channel-group 1 mode active",
-                "exit",
-                "int port-channel 1",
-                "switchport trunk enc dot1",
+                "int range g0/1-3, g1/0",
+                "switchport trunk enc dot1Q",
                 "switchport mode trunk",
                 "switchport trunk allowed vlan 10,20,30,40,99",
-                "end"
+                "end",
+                "write memory"
             ]
 
-            output = net_con.send_config_set(cmd)
 
+            output = net_conn.send_config_set(cmd)
             time.sleep(1)
-
             print(output)
 
-            net_con.disconnect()
+            net_conn.disconnect()
         else:
-            print(f"{ip} non configurable en etherchannel")
-
+            print(f"{ip} not belongs to the configuration")
     except NetMikoAuthenticationException:
-        print(f"Failed to authenticate to the host {ip}")
+        print(f"Failed to authenticate to {ip}")
     except NetMikoTimeoutException:
         print(f"Host unreachable")
